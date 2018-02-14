3a2709de8215dabac00597a78f7b4854a522ae10
Bastian Pawlik <qwasyx.github.20.cooper@neverbox.com>
2018-02-14 20:59:33 +0100

    fix getRooms functionality. Added actors for more samples.

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ var db = require('lib/db');
 var server = restify.createServer();
 
 function getActor(req, res, next) {
-  var actor = db.getActors({
-    building: req.params.building,
-    room: req.params.room,
-    name: req.params.name
-  });
-  res.send(actor);
-  return next();
+	  var actor = db.getActors({
+		      building: req.params.building,
+		      room: req.params.room,
+		      name: req.params.name
+		    });
+	  res.send(actor);
+	  return next();
+}
+
+function getRooms(req, res, next) {
+	  var room= db.getRooms({
+		      building: req.params.building
+		    });
+	  res.send(room);
+	  return next();
 }
 server.get('/:building/:room/:name', getActor);
+server.get('/:building', getRooms);
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+	  console.log('%s listening at %s', server.name, server.url);
 });
